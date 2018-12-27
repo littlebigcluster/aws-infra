19ff5ff6fbc1a65923f0c8cdd8214098e1348e6a
llacroze <llacroze@anybox.fr>
2018-12-27 12:13:25 +0100

    modif packer script

--- swarm-cluster/Staging-packer/2-create-AMI-manager.sh
+++ swarm-cluster/Staging-packer/2-create-AMI-manager.sh
@@ -12,12 +12,13 @@ read VPCID
 SUBNETIDS=$(aws ec2 describe-subnets --output text --filters "Name=vpc-id,Values=$VPCID" \
 --query 'Subnets[*].{SubnetId:SubnetId,Name:Tags[?Key==`Name`].Value|[0],CidrBlock:CidrBlock}' | grep public | awk '{print $3}')
 SUBNETID=($(echo $SUBNETIDS | tr " " "\n"))
-echo "Subnetid public: $SUBNETID"
-# list security group:
+echo "Subnetid public: ${SUBNETID[0]}"
 
+
+# list security group:
 SGROUP=$(aws ec2 describe-security-groups --output text --filters "Name=vpc-id,Values=$VPCID" \
---query 'SecurityGroups[*].{GroupId:GroupId,Description:Description}' grep "default VPC security group" | awk '{print $5}'
-echo "Quel Security group ?")
+--query 'SecurityGroups[*].{GroupId:GroupId,Description:Description}' | grep "default VPC security group" | awk '{print $5}')
+echo "Quel Security group ?"
 echo "Security group: $SGROUP"
 
 EFSID=$(cat "efs-id.txt")
@@ -26,8 +27,9 @@ sed -i s/fs-XXXXXX/$EFSID/ docker-aws-debian-manager.json
 
 packer build \
        -var aws_vpc_id=$VPCID \
-       -var aws_subnet_id=${SUBNETID[0] \
+       -var aws_subnet_id=${SUBNETID[0]} \
        -var aws_security_group_id=$SGROUP \
+       -var aws_default_region='eu-west-1' \
        docker-aws-debian-manager.json 2>&1 | tee output-build-manager.txt
 
 tail -2 output-build-manager.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ami-manager.txt
--- swarm-cluster/Staging-packer/2-create-AMI-worker.sh
+++ swarm-cluster/Staging-packer/2-create-AMI-worker.sh
@@ -16,8 +16,7 @@ echo "Subnetid public: $SUBNETID"
 # list security group:
 
 SGROUP=$(aws ec2 describe-security-groups --output text --filters "Name=vpc-id,Values=$VPCID" \
---query 'SecurityGroups[*].{GroupId:GroupId,Description:Description}' grep "default VPC security group" | awk '{print $5}'
-echo "Quel Security group ?")
+--query 'SecurityGroups[*].{GroupId:GroupId,Description:Description}' | grep "default VPC security group" | awk '{print $5}')
 echo "Security group: $SGROUP"
 
 EFSID=$(cat "efs-id.txt")
@@ -27,8 +26,9 @@ sed -i s/fs-XXXXXX/$EFSID/ docker-aws-debian-worker.json
 
 packer build \
        -var aws_vpc_id=$VPCID \
-       -var aws_subnet_id=${SUBNETID[0] \
+       -var aws_subnet_id=${SUBNETID[0]} \
        -var aws_security_group_id=$SGROUP \
+       -var aws_default_region='eu-west-1' \
        docker-aws-debian-worker.json 2>&1 | tee output-build-worker.txt
 
 tail -2 output-build-worker.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ami-worker.txt
